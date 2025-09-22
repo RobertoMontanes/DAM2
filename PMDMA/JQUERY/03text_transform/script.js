b622f7bca0b76eb71fee059a61ae3eda443ae7ef
Pablo Garcia - Clase <garcia.mapab24@triana.salesianos.edu>
2025-09-22 10:09:52 +0200

    LISOT

---
+++ PMDMA/JQUERY/03text_transform/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+
+    var output = ""
+
+    $(document).on("click", "#btnAction", function () {
+        var input = $("#textInput").val()
+        var selectVal = $("#selectAction").val();
+
+        console.log(input);
+        
+        if (input == "") {            
+            alert("Tienes que escribir algun contenido")
+        } else {
+            if (selectVal == "null") {
+                alert("Debes seleccionar una opcion valida.")
+            } else {
+                if (selectVal == "upper") {
+                    console.log(input);
+                    output = upperCase(input)
+                } else if (selectVal == "lower") {
+                    output = lowerCase(input)
+                }
+                                
+                $("#textOutput").val(output)
+
+            }
+        }
+    });
+
+})
+
+function upperCase(input) {
+    return input.toUpperCase()
+}
+
+function lowerCase(input) {
+    return input.toLowerCase()
+}
