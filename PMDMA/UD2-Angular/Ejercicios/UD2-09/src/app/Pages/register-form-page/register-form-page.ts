aa1d6d8f9b7e4d0fb71efcd48543f848d28dc083
Pablo Garcia - Clase <garcia.mapab24@triana.salesianos.edu>
2025-11-11 13:34:46 +0100

    Validators

--- PMDMA/UD2-Angular/Ejercicios/UD2-09/src/app/Pages/register-form-page/register-form-page.ts
+++ PMDMA/UD2-Angular/Ejercicios/UD2-09/src/app/Pages/register-form-page/register-form-page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-form-page',
@@ -10,15 +10,15 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 export class RegisterFormPage {
 
   registerFormGroup = new FormGroup({
-    nombreFormControl: new FormControl(''),
-    apellidosFormControl: new FormControl(''),
-    nifFormControl: new FormControl(''),
-    emailFormControl: new FormControl(''),
-    telefonoFormControl: new FormControl(''),
-    sexoFormControl: new FormControl(''),
-    origenFormControl: new FormControl(''),
-    passwordFormControl: new FormControl(''),
-    confirmarPasswordFormControl: new FormControl(''),
+    nombreFormControl: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    apellidosFormControl: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    nifFormControl: new FormControl('', [Validators.required, Validators.pattern(/^\d{8}[A-Z]$/)]),
+    emailFormControl: new FormControl('', [Validators.required, Validators.email]),
+    telefonoFormControl: new FormControl('', [Validators.required, Validators.pattern(/^\d{9}$/)]),
+    sexoFormControl: new FormControl('', Validators.required),
+    origenFormControl: new FormControl('', Validators.required),
+    passwordFormControl: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    confirmarPasswordFormControl: new FormControl('', Validators.required),
   });
 
   onSubmit() {
