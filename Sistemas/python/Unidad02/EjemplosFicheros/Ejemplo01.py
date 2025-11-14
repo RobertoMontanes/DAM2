30ec750b183129a9f3823b1b70a0e297905480ba
Pablo Garcia - Clase <garcia.mapab24@triana.salesianos.edu>
2025-11-14 14:28:20 +0100

    asd

--- Sistemas/python/Unidad02/EjemplosFicheros/Ejemplo01.py
+++ Sistemas/python/Unidad02/EjemplosFicheros/Ejemplo01.py
@@ -1,1 +1,14 @@
-f = open("files/temps.txt", "r")
+f = open("files/temps.txt")
+print(f.read())
+
+print("BUSQUEDA LINEA A LINEA")
+
+f.seek(0)
+linea1 = f.readline(1)
+print(linea1)
+
+print("BUSQUEDA for")
+
+f.seek(0)
+for linea in f.readlines():
+    print(linea, end='')
