82aee4fe056fabcb2d492ce23a8975e30b4a130e
Pablo Garcia - Clase <garcia.mapab24@triana.salesianos.edu>
2025-11-19 14:33:04 +0100

    asd

---
+++ PMDMA/UD2-Angular/Ejercicios/UD2-10/src/app/interceptors/api-key-interceptor.ts
@@ -0,0 +1,14 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+
+  const apiKey = '510cf67d07a0a046a32480eb9098b27b'
+  const apiToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MTBjZjY3ZDA3YTBhMDQ2YTMyNDgwZWI5MDk4YjI3YiIsIm5iZiI6MTc2MzM2NzQ4Mi4zMjQsInN1YiI6IjY5MWFkYTNhMDM5NGQ1YWQ0MjZkOTI1NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uD8kgDmUK2TeT41R5r50ZpqdOlutm3-q4G4nAXz2naQ'
+  
+
+export const apiKeyInterceptor: HttpInterceptorFn = (req, next) => {
+
+  const reqWithHeader = req.clone({
+    headers: req.headers.set('Authorization', `Bearer ${apiToken}`),
+  });
+
+  return next(reqWithHeader);
+};
