f762f71e57aa220b0a232ce21a424da3004682f2
Pablo Garcia - Clase <garcia.mapab24@triana.salesianos.edu>
2026-01-22 09:00:53 +0100

    asad

---
+++ Sistemas/python/Unidad04/Ejercicios/ejercicio02_tipo_examen.py
@@ -0,0 +1,14 @@
+# 2. Me gustan los animales. Aunque no estoy muy de acuerdo con los zoológicos, crea un programa que ayude
+# a calcular el coste anual para alimentar distintos tipos de animales.
+# Hay Animales, en general, que comen hierba, fruta, etc. En este caso el coste se realiza multiplicando una
+# cantidad de dinero al día por el número de días del año. Por otro lado, hay animales con comida especial:
+# - Osos: 2 veces a la semana comen carne o pescado por lo que, al coste normal (por frutas, verduras...), hay
+# que sumarle el coste de esto.
+# - Serpientes: Comen insectos cuyo coste es por el número de insectos que se le dan por lo que cuesta cada
+# insecto, pero solo comen dos veces a la semana.
+# Crea una clase Zoo con métodos que calculen:
+# - Coste de una serie de animales pasando como parámetro, la cantidad de cada uno de estos tipos.
+# - Calcular el descuento que nos hace la empresa suministradora, si gastamos más de una cantidad dada como
+# parámetro entre todos los animales de una lista.
+# - Gasto del zoo solo en osos.
+# Crea un main de prueba.
